3dfcedc078f856a4f39b35bbe90f350ec3b946d2
chandini-ai <chandinigurram1111@gmail.com>
2026-03-29 11:07:07 +0530

    Your commit message here

--- script.js
+++ script.js
@@ -16,15 +16,7 @@ function handleStudentLogin(email, password) { return loginStudent(email, passwo
 function handleSchoolLogin(udise, password) { return loginSchool(udise, password); }
 
 // ---------- Firebase auth ----------
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCj83HXKPSSzbBzQaG7jZAYBv4VzxEl8rI",
-  authDomain: "student-admission-system-b64a7.firebaseapp.com",
-  projectId: "student-admission-system-b64a7",
-  storageBucket: "student-admission-system-b64a7.firebasestorage.app",
-  messagingSenderId: "324694363467",
-  appId: "1:324694363467:web:0f52ef5119f2bb2f66ecce",
-  measurementId: "G-HJK4PGDGY1"
-};
+
 
 let firebaseAuthInitPromise = null;
 
