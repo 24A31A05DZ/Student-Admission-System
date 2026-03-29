5d78d5d0be94cce331f1c428b2544ee6c2d7cf17
Gurram Chandini <chandinigurram1111@gmail.com>
2026-03-29 11:49:49 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCj83HXKPSSzbBzQaG7jZAYBv4VzxEl8rI",
-  authDomain: "student-admission-system-b64a7.firebaseapp.com",
-  projectId: "student-admission-system-b64a7",
-  storageBucket: "student-admission-system-b64a7.firebasestorage.app",
-  messagingSenderId: "324694363467",
-  appId: "1:324694363467:web:049dabce9f2761c966ecce",
-  measurementId: "G-F8XRM2YXL3"
-};
